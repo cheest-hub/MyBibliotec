17a5fc88a54b7605e29e836b6d565846c129caa8
nelton <nelcon_001@hotmail.com>
2026-03-20 16:44:09 -0400

    estructura inial do projeto MyBibliotec

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer">
+      </section>
+    </>
+  )
+}
+
+export default App
